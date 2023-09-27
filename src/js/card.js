81ace81c444b71bf29bf8bc1e7d292660bbb7bd4
okwurt <okwurt@gmail.com>
2023-09-26 23:44:21 -0500

    Fixed code

--- src/js/card.js
+++
@@ -1,4 +0,0 @@
-function createPokemonCard(id, name, type, game, ball, nickname = null, gender = null) {
-    return new PokemonCard(id, name, type, game, ball, nickname, gender);
-}
-
--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ import { loadStaticData } from "./pokemonData.js";
 import { setupEventListeners } from "./eventHandling.js";
 import { renderPokemonCollection, displayMergedData, renderPokemonDetails, renderPokemonDetailsbyName, createPokemonCard } from "./uiRendering.js";
 import { addOrUpdatePokemonToCollection, removePokemonFromCollection, switchCollection } from "./collectionManagement.js";
-import { createPokemonCard } from "./card.js";
 
 // Initialize
 initialize();
