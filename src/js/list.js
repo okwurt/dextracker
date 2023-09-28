e91d1ae75cf8a3b0bb0129a35fdd2b498b4b28e5
okwurt <okwurt@gmail.com>
2023-09-28 16:48:09 -0500

    Updated SV and code

--- src/js/list.js
+++ src/js/list.js
@@ -5,69 +5,32 @@ export function renderPokemonList(pokemonData) {
     listContainer.innerHTML = '';  // Clear any existing content
 
     const list = document.createElement('ul');
-    list.className = 'pokemon-list';
+    list.className = 'pokemon-item';
 
     pokemonData.forEach(pokemon => {
         const listItem = document.createElement('li');
 
         // Construct inner HTML for each Pokemon
         listItem.innerHTML = `
-            <div class="pokemon-item">
-                <div class="column-id">${pokemon.id}</div>
-                <img src="./sprites/icons/${pokemon.name.toLowerCase()}.png" alt="${pokemon.name} sprite">
+            <div class="pokemon-card">
+                <div class="poke-id">${pokemon.id}</div>
+                <img src="./sprites/games/scarlet-violet/normal/${pokemon.name.toLowerCase()}.png" alt="${
+          pokemon.name
+        } sprite">
                 <div class="pokemon-name">${pokemon.name}</div>
                 <div class="pokemon-type">
-                ${pokemon.type.map(type => 
-                    `<span class="type-badge ${type.toLowerCase()}">${type}</span>`
-                ).join('')}
+                ${pokemon.type
+                  .map(
+                    (type) =>
+                      `<span class="type-badge ${type.toLowerCase()}">${type}</span>`
+                  )
+                  .join("")}
                 </div>
-                <div class="pokemon-abilities">
-                ${pokemon.abilities.join(', ')}
-                </div>
-                <div class="pokemon-base-stats">
-                    <ul>
-                        ${Object.entries(pokemon.baseStats).map(([stat, value]) => 
-                            `<li>${stat.toUpperCase()}: ${value}</li>`
-                        ).join('')}
-                    </ul>
-                </div>
-                <div class="pokemon-egg-groups"> ${pokemon.eggGroups.join(', ')}</div>
             </div>
         `;
-
-        if (pokemon.forms && pokemon.forms.length > 0) {
-            const formsCarousel = renderFormsCarousel(pokemon.forms);
-
-            listItem.appendChild(formsCarousel);
-        }
 
         list.appendChild(listItem);
     });
 
     listContainer.appendChild(list);
-}
-
-export function renderFormsCarousel(pokemonData) {
-    const carousel = document.createElement('div');
-    carousel.className = 'forms-carousel';
-
-    const nonBattleForms = pokemonData.filter(form => !form.isBattleForm);
-
-    nonBattleForms.forEach(form => {
-        let slide = document.createElement('div');
-        slide.className = 'forms-slide';
-
-        slide.innerHTML = `
-            <img src="./sprites/icons/${form.formName.toLowerCase()}.png" alt="${form.displayName} sprite">
-            <div class="form-name">${form.displayName}</div>
-            <div class="form-type">
-                ${form.type.map(type => 
-                    `<span class="type-badge ${type.toLowerCase()}">${type}</span>`
-                ).join('')}
-            </div>
-        `;
-        carousel.appendChild(slide);
-    }); 
-
-    return carousel;
 }
